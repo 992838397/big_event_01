b1f20347fe40a33291347fee6688758ed64111cf
linziyun <992838397@qq.com>
2021-03-15 10:03:34 +0800

    首页功能完成

--- assets/js/baseAPi.js
+++ assets/js/baseAPi.js
@@ -5,9 +5,32 @@ $(function () {
     // 拦截所有Ajax请求:get post $ajax
     //处理参数：
     $.ajaxPrefilter(function (options) {
-        // 进行字符串拼接
+        // 1.自动为每一个url添加前缀 进行字符串拼接
         // console.log('http://api-breakingnews-web.itheima.net' + options.url);
         options.url = baseURL + options.url
+
+
+        //2. 身份证验证            授权从本地获取身份验证 获取 token
+        if (options.url.indexOf('/my/') != -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+
+
+
+            options.complete = function (res) {
+                console.log(res.responseJSON);
+                var obj = res.responseJSON;
+                if (obj.status == 1 && obj.message == '身份认证失败！') {
+                    // 1.清空本地token   token是身份验证参数,唯一性
+                    localStorage.removeItem('token');
+                    // 2.页面跳转
+                    location.href = '/login.html';
+                }
+
+            }
+
+        }
     })
 
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,22 @@
 $(function () {
-    // 获取用户信息
+    //1. 获取用户信息
     getUserInof();
+
+
+    // 2.  退出功能
+    var layer = layui.layer;
+    $("#btnLogout").on('click', function () {
+        //eg1
+        layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            //1.清空本地的token  跳转页面
+            localStorage.removeItem('token');
+            //2.页面跳转
+            location.href = '/login.html'
+            // 3.关闭弹窗
+            layer.close(index);
+        });
+    })
 });
 
 // 获取用户信息
@@ -10,12 +26,12 @@ function getUserInof() {
     $.ajax({
         //   type: '',
         url: '/my/userinfo',
-        //   data: {},
-        headers: {
-            // 授权从本地获取token  
-            //之前存储到本地去了
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // 身份验证
+        // headers: {
+        // 授权从本地获取token  
+        //之前存储到本地去了
+        // Authorization: localStorage.getItem('token') || ''
+        // },
         success: (res) => {
             // console.log(res);
             if (res.status !== 0) {
@@ -24,12 +40,24 @@ function getUserInof() {
             // 请求成功
             randerAvatar(res.data);
 
-        }
+        },
+        // 登陆拦截,不管成功还是失败都会触发这个
+        // complete: function (res) {
+        //     console.log(res.responseJSON);
+        //     var obj = res.responseJSON;
+        //     if (obj.status == 1 && obj.message == '身份认证失败！') {
+        //         // 1.清空本地token   token是身份验证参数,唯一性
+        //         localStorage.removeItem('token');
+        //         // 2.页面跳转
+        //         location.href = '/login.html';
+        //     }
+        // },
     })
 }
 
 
-// 封装一个渲染头像图片的函数
+// 封装
+// 渲染头像图片的函数
 function randerAvatar(user) {
     // 1.渲染名称 (nickname优先,如果没有,就用username)
     console.log(user);
